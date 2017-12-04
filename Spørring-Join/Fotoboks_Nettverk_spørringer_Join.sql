fdf781a6820be77a63fcd7c7b9c2131cd7c41517
Gullik <34219356+goldoak101@users.noreply.github.com>
2017-12-04 18:07:24 +0100

    Update Fotoboks_Nettverk_spørringer_Join.sql

--- Spørring-Join/Fotoboks_Nettverk_spørringer_Join.sql
+++ Spørring-Join/Fotoboks_Nettverk_spørringer_Join.sql
@@ -7,3 +7,10 @@ select * from produkt inner join kategori;
 
 select * from produkt inner join kategori on produkt.kategori_ID = kategori.kategori_ID;
 
+select produkt.navn, kategori.navn from produkt inner join kategori on produkt.kategori_ID = kategori.kategori_ID;
+
+select produkt.navn, kategori.navn from produkt right join kategori on produkt.kategori_ID = kategori.kategori_ID;
+
+select produkt.navn, kategori.navn from produkt left join kategori on produkt.kategori_ID = kategori.kategori_ID;
+-- dette vil ikke gjøre noe utslag siden vi ikke har noen produkter som ikke har en kategorti.
+
