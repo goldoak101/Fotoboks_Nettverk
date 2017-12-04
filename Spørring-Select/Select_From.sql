ed74bf81b812166a0fbcde803468083788bc6d99
Gullik <34219356+goldoak101@users.noreply.github.com>
2017-12-04 21:52:13 +0100

    Add files via upload

---
+++ Spørring-Select/Select_From.sql
@@ -0,0 +1,21 @@
+create schema if not exists Fotoboks_Nettverk;
+use Fotoboks_Nettverk;
+-- Dette er er for å lage og deretter bruke bruke databasen som er opprettet.
+
+
+
+select pris, navn from produkt;
+
+
+####################################
+
+select * from produkt;
+
+
+#################################### -- dette vil bli behandlet på samme måte
+
+select * 
+
+from produkt; 
+
+####################################
